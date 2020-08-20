3bc8b0ebda623be2e26470090f170cd9bf80a0c6
jude@yourtechy.com <jude@yourtechy.com>
2020-08-20 16:13:17 -0700

    RestError interface

--- rest_errors/rest_errors.go
+++ rest_errors/rest_errors.go
@@ -1,43 +1,113 @@
 package rest_errors
 
-import "net/http"
+import (
+	"net/http"
+)
 
-type RestError struct {
-	Message string
-	Status  int
+type RestErrorInterfaceInterface interface {
+	Error() string
+	Message() string
+	Status() int
+	Causes() []interface{}
 }
 
-func UnauthorizedError(message string) *RestError {
-	return &RestError{
-		Message: message,
-		Status:  http.StatusUnauthorized,
+type restError struct {
+	message string
+	status  int
+	error   string
+	causes  []interface{}
+}
+
+func (e *restError) Error() string {
+	return e.error
+}
+
+func (e *restError) Message() string {
+	return e.message
+}
+
+func (e *restError) Status() int {
+	return e.status
+}
+
+func (e *restError) Causes() []interface{} {
+	return e.causes
+}
+
+//
+// CustomError
+//
+func RestError(
+	mesage string,
+	status int,
+	err string,
+	causes []interface{},
+) RestErrorInterface {
+	return restError{
+		message: message,
+		status:  status,
+		error:   err,
+		causes:  causes,
 	}
 }
 
-func NotFoundError(message string) *RestError {
-	return &RestError{
-		Message: message,
-		Status:  http.StatusNotFound,
+//
+// UnauthorizedError
+//
+func UnauthorizedError(message string) RestErrorInterface {
+	return restError{
+		message: message,
+		status:  http.StatusUnauthorized,
+		error:   "Unauthorized",
 	}
 }
 
-func BadRequestError(message string) *RestError {
-	return &RestError{
-		Message: message,
-		Status:  http.StatusBadRequest,
+//
+// NotFoundError
+//
+func NotFoundError(message string) RestErrorInterface {
+	return restError{
+		message: message,
+		status:  http.StatusNotFound,
+		error:   "Not Found",
 	}
 }
 
-func InternalServerError(message string) *RestError {
-	return &RestError{
-		Message: message,
-		Status:  http.StatusInternalServerError,
+//
+// BadRequestError
+//
+func BadRequestError(message string) RestErrorInterface {
+	return restError{
+		message: message,
+		status:  http.StatusBadRequest,
+		error:   "Bad Request",
 	}
 }
 
-func InvalidParameterError(message string) *RestError {
-	return &RestError{
-		Message: message,
-		Status:  http.StatusPreconditionFailed,
+//
+// InvalidParameterError
+//
+func InvalidParameterError(message string) RestErrorInterface {
+	return restError{
+		message: message,
+		status:  http.StatusPreconditionFailed,
+		error:   "Invalid Parameter",
 	}
+}
+
+//
+// InternalServerError
+//
+func InternalServerError(message string, err error) RestErrorInterface {
+	res = restError{
+		message: message,
+		status:  http.StatusInternalServerError,
+		error:   "Internal Server Error",
+	}
+
+	if err != nil {
+		res.causes = append(res.causes, err.Error())
+	}
+
+	return res
 }
--- rest_errors/rest_errors_test.go
+++ rest_errors/rest_errors_test.go
@@ -7,13 +7,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestInternalServerError(t *testing.T) {
+func TestUnauthorizedError(t *testing.T) {
 
-	err := InternalServerError("Internal server error test")
+	err := UnauthorizedError("Internal server error test")
 
 	assert.NotNil(t, err)
-	assert.EqualValues(t, http.StatusInternalServerError, err.Status)
-	assert.EqualValues(t, "Internal server error test", err.Message)
+	assert.EqualValues(t, http.StatusUnauthorized, err.Status())
+	assert.EqualValues(t, "Internal server error test", err.Message())
+}
+
+func TestInternalServerError(t *testing.T) {
+
+	err := InternalServerError("Internal server error test", nil)
+
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.Status())
+	assert.EqualValues(t, "Internal server error test", err.Message())
 }
 
 func TestBadRequestError(t *testing.T) {
@@ -21,8 +30,8 @@ func TestBadRequestError(t *testing.T) {
 	err := BadRequestError("Bad request error test")
 
 	assert.NotNil(t, err)
-	assert.EqualValues(t, http.StatusBadRequest, err.Status)
-	assert.EqualValues(t, "Bad request error test", err.Message)
+	assert.EqualValues(t, http.StatusBadRequest, err.Status())
+	assert.EqualValues(t, "Bad request error test", err.Message())
 }
 
 func TestNotFoundError(t *testing.T) {
@@ -30,8 +39,8 @@ func TestNotFoundError(t *testing.T) {
 	err := NotFoundError("Not found error test")
 
 	assert.NotNil(t, err)
-	assert.EqualValues(t, http.StatusNotFound, err.Status)
-	assert.EqualValues(t, "Not found error test", err.Message)
+	assert.EqualValues(t, http.StatusNotFound, err.Status())
+	assert.EqualValues(t, "Not found error test", err.Message())
 }
 
 func TestInvalidParameterError(t *testing.T) {
@@ -39,6 +48,6 @@ func TestInvalidParameterError(t *testing.T) {
 	err := InvalidParameterError("Invalid parameter error test")
 
 	assert.NotNil(t, err)
-	assert.EqualValues(t, http.StatusPreconditionFailed, err.Status)
-	assert.EqualValues(t, "Invalid parameter error test", err.Message)
+	assert.EqualValues(t, http.StatusPreconditionFailed, err.Status())
+	assert.EqualValues(t, "Invalid parameter error test", err.Message())
 }
